9087a78061f7e2321c416b8724861a7fa1eaf9e4
Jacob Gunther <jacobgunther12@gmail.com>
2018-04-14 17:43:37 -0500

    Fixed bugs in test builds

--- test/bot.js
+++ test/bot.js
@@ -2,12 +2,12 @@ const Client = require('../src/Client');
 
 const bot = new Client(process.env.TEST_TOKEN, {});
 
-bot.on('ready', () => {
+bot.bot.on('ready', () => {
 	if (bot.user.id !== '434843046304022528') return process.exit(1);
 	process.exit(0);
 });
 
-bot.on('disconnect', () => {
+bot.bot.on('disconnect', () => {
 	process.exit(1);
 });
 
