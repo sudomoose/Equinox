c55e84044844b7d038c94f73d666d99f38e30cee
Jacob Gunther <jacobgunther12@gmail.com>
2018-04-14 17:52:15 -0500

    Fixed bug

--- test/bot.js
+++ test/bot.js
@@ -3,7 +3,7 @@ const Client = require('../src/Client');
 const bot = new Client(process.env.TEST_TOKEN, {});
 
 bot.bot.on('ready', () => {
-	if (bot.user.id !== '434843046304022528') return process.exit(1);
+	if (bot.bot.user.id !== '434843046304022528') return process.exit(1);
 	process.exit(0);
 });
 
