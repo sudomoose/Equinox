e2228c5e5be9eec6cee2a5fdf774b3d399f54a56
Jacob Gunther <jacobgunther12@gmail.com>
2018-04-14 17:34:08 -0500

    Added test bot

---
+++ test/bot.js
@@ -0,0 +1,14 @@
+const Client = require('../src/Client');
+
+const bot = new Client(process.env.TEST_TOKEN, {});
+
+bot.on('ready', () => {
+	if (bot.user.id !== '434843046304022528') return process.exit(1);
+	process.exit(0);
+});
+
+bot.on('disconnect', () => {
+	process.exit(1);
+});
+
+bot.launch();
