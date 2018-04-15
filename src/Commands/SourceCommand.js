f00934739cd2547cc9188efd9c10066b9ca8b2b3
Jacob Gunther <jacobgunther12@gmail.com>
2018-04-15 11:34:36 -0500

    Added source command

---
+++ src/Commands/SourceCommand.js
@@ -0,0 +1,25 @@
+const BaseCommand = require('../Structure/BaseCommand');
+const config = require('../config.json');
+
+class Balance extends BaseCommand {
+	constructor(bot, r) {
+		super({
+			command: 'source',
+			aliases: [
+				'src'
+			],
+			description: 'Gets the source of the bot on GitHub..',
+			category: 'Utility',
+			usage: 'source',
+			hidden: false
+		});
+		this.bot = bot;
+		this.r = r;
+	}
+
+	execute(msg) {
+		msg.channel.createMessage(':gear:   **»**   ' + config.links.github);
+	}
+}
+
+module.exports = Balance;
