f48f9025ef06f1478c9558c07489645b3f82c6b6
Jacob Gunther <jacobgunther12@gmail.com>
2018-04-30 16:51:27 -0500

    Added raffle command

---
+++ src/Commands/RaffleCommand.js
@@ -0,0 +1,24 @@
+const BaseCommand = require('../Structure/BaseCommand');
+
+class Raffle extends BaseCommand {
+	constructor(bot, r) {
+		super({
+			command: 'raffle',
+			aliases: [],
+			description: 'Randomly selects a user from the server.',
+			category: 'Utility',
+			usage: 'raffle',
+			hidden: false
+		});
+		this.bot = bot;
+		this.r = r;
+	}
+
+	execute(msg) {
+		if (!msg.channel.guild) return msg.channel.createMessage(':no_entry_sign:   **»**   This command cannot be used in a direct message.');
+		const member = Array.from(msg.channel.guild.members.values())[Math.floor(Math.random() * msg.channel.guild.members.size)];
+		msg.channel.createMessage(':tickets:   **»**   `' + member.user.username + '#' + member.user.discriminator + ' (' + member.user.id + ')`');
+	}
+}
+
+module.exports = Raffle;
