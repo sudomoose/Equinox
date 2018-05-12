f4c9dcaf35e56648974b7d08146af94e2be3248c
Jacob Gunther <jacobgunther12@gmail.com>
2018-05-12 18:51:16 -0500

    Fixed bug

--- src/Events/RawWebSocket.js
+++ src/Events/RawWebSocket.js
@@ -1,6 +1,6 @@
 module.exports = (bot, r, metrics) => {
 	bot.on('rawWS', (payload) => {
 		metrics.increment('events.raw', 1);
-		metrics.increment('events.top', [ 'event:' + String(payload.t) ]);
+		metrics.increment('events.top', 1, [ 'event:' + String(payload.t) ]);
 	});
 };
