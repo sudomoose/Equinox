6562e4a93765075f0482c4d212ba2f1235a252e1
Jacob Gunther <jacobgunther12@gmail.com>
2018-06-27 23:08:50 -0500

    Fixed incorrect interval in restart

--- src/Commands/RestartCommand.js
+++ src/Commands/RestartCommand.js
@@ -27,22 +27,22 @@ class Restart extends BaseCommand {
 		this.r.table('developers').get(msg.author.id).run(async (error, developer) => {
 			if (error) return handleDatabaseError(error, msg);
 			if (!developer) return msg.channel.createMessage(':no_entry_sign:   **»**   You do not have permission to run this command.');
-			this.r.table('intervals').insert({
-				id: 'restart',
-				channelID: msg.channel.id,
-				userID: msg.author.id,
-				timestamp: Date.now()
-			}, { conflict: 'replace' }).run((error) => {
-				if (error) return handleDatabaseError(error, msg);
-				msg.channel.createMessage(':arrows_counterclockwise:   **»**   Cleaning up cached data before exiting...');
-				this.bot.runQueuedQueries();
-				setTimeout(() => {
+			msg.channel.createMessage(':arrows_counterclockwise:   **»**   Cleaning up cached data before exiting...');
+			this.bot.runQueuedQueries();
+			setTimeout(() => {
+				this.r.table('intervals').insert({
+					id: 'restart',
+					channelID: msg.channel.id,
+					userID: msg.author.id,
+					timestamp: Date.now()
+				}, { conflict: 'replace' }).run((error) => {
+					if (error) return handleDatabaseError(error, msg);
 					msg.channel.createMessage(':arrows_counterclockwise:   **»**   Restarting the PM2 process...');
 					child_process.exec('pm2 restart ' + config.pm2_process, (error) => {
 						if (error) msg.channel.createMessage('```\n' + error + '```');
 					});
-				}, 5000);
-			});
+				});
+			}, 5000);
 		});
 	}
 }
