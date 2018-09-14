d62d572caa6138120a64b1dfcdcfb3b0eefc08ba
Jacob Gunther <jacobgunther12@gmail.com>
2018-09-13 19:11:00 -0500

    Fixed lavalink port

--- src/Util/resolveTrack.js
+++ src/Util/resolveTrack.js
@@ -3,7 +3,7 @@ const config = require('../config.json');
 
 module.exports = (query) => {
 	return new Promise((resolve, reject) => {
-		snekfetch.get('http://' + config.lavalink.nodes[0].host + ':2333/loadtracks?identifier=' + query).set('Authorization', config.lavalink.nodes[0].password).then((result) => {
+		snekfetch.get('http://' + config.lavalink.nodes[0].host + ':8088/loadtracks?identifier=' + query).set('Authorization', config.lavalink.nodes[0].password).then((result) => {
 			resolve(result.body);
 		}).catch((error) => reject(error));
 	});
